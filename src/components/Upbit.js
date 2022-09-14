8c5f60bc9a550f3c88564de10aa3e340a69f374a
ichenny <ichenny.lee@gmail.com>
2022-09-14 20:28:22 +0900

    현재가 불러오기

--- src/components/Upbit.js
+++ src/components/Upbit.js
@@ -15,21 +15,27 @@ function Upbit() {
     setLoading(false);
   };
 
-  // 코인 현재 가격 가져오기
-  const getPrice = async (name) => {
-    const apiUrl = "https://api.upbit.com/v1/ticker?markets=";
-    const priceUrl = apiUrl + name;
+  // 코인 이름 배열
 
-    const response = await fetch(priceUrl);
+  // const coinName = coins.map((el) => el.market).join();
+
+  // 코인 현재 가격 가져오기
+  const getPrice = async () => {
+    let url =
+      "https://api.upbit.com/v1/ticker?markets=KRW-BTC,KRW-ETH,KRW-NEO,KRW-MTL,KRW-XRP,KRW-ETC,KRW-OMG,KRW-SNT,KRW-WAVES,KRW-XEM,KRW-QTUM,KRW-LSK,KRW-STEEM,KRW-XLM,KRW-ARDR,KRW-ARK,KRW-STORJ,KRW-GRS,KRW-REP,KRW-ADA,KRW-SBD,KRW-POWR,KRW-BTG,KRW-ICX,KRW-EOS,KRW-TRX,KRW-SC,KRW-ONT,KRW-ZIL,KRW-POLY,KRW-ZRX,KRW-LOOM,KRW-BCH,KRW-BAT,KRW-IOST,KRW-RFR,KRW-CVC,KRW-IQ,KRW-IOTA,KRW-MFT,KRW-ONG,KRW-GAS,KRW-UPP,KRW-ELF,KRW-KNC,KRW-BSV,KRW-THETA,KRW-QKC,KRW-BTT,KRW-MOC,KRW-ENJ,KRW-TFUEL,KRW-MANA,KRW-ANKR,KRW-AERGO,KRW-ATOM,KRW-TT,KRW-CRE,KRW-MBL,KRW-WAXP,KRW-HBAR,KRW-MED,KRW-MLK,KRW-STPT,KRW-ORBS,KRW-VET,KRW-CHZ,KRW-STMX,KRW-DKA,KRW-HIVE,KRW-KAVA,KRW-AHT,KRW-LINK,KRW-XTZ,KRW-BORA,KRW-JST,KRW-CRO,KRW-TON,KRW-SXP,KRW-HUNT,KRW-PLA,KRW-DOT,KRW-SRM,KRW-MVL,KRW-STRAX,KRW-AQT,KRW-GLM,KRW-SSX,KRW-META,KRW-FCT2,KRW-CBK,KRW-SAND,KRW-HUM,KRW-DOGE,KRW-STRK,KRW-PUNDIX,KRW-FLOW,KRW-DAWN,KRW-AXS,KRW-STX,KRW-XEC,KRW-SOL,KRW-MATIC,KRW-NU,KRW-AAVE,KRW-1INCH,KRW-ALGO,KRW-NEAR,KRW-WEMIX,KRW-AVAX,KRW-T,KRW-CELO,KRW-GMT";
+
+    const response = await fetch(url);
     const json = await response.json();
     setPrice(json);
   };
 
   useEffect(() => {
     getCoins();
-    getPrice("KRW-BTC");
+    getPrice();
   }, []);
-  console.log(price);
+  // console.log(price);
+  let test = price.filter((el) => el.market === "KRW-BTC")[0];
+  console.log(test["trade_price"]);
 
   return (
     <div>
@@ -41,7 +47,12 @@ function Upbit() {
         <ul>
           {coins.map((coin) => (
             <li key={coin.market}>
-              {coin.market} {coin.korean_name} {coin.english_name}
+              {coin.market} {coin.korean_name} {coin.english_name}{" "}
+              {
+                price.filter((el) => el.market === coin.market)[0][
+                  "trade_price"
+                ]
+              }
             </li>
           ))}
         </ul>
