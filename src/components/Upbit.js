88f43f9c60ac5fefab1de04a1a2ffb35f860e399
YeryunJung <2bites@naver.com>
2022-09-14 20:38:23 +0900

    현재가 불러오기

--- src/components/Upbit.js
+++ src/components/Upbit.js
@@ -9,27 +9,36 @@ function Upbit() {
   const getCoins = async () => {
     const response = await fetch("https://api.upbit.com/v1/market/all");
     const json = await response.json();
-    const krw = json.filter((el) => el.market.includes("KRW"));
+    const krw = await json.filter((el) => el.market.includes("KRW"));
 
     setCoins(krw);
     setLoading(false);
   };
 
+  const krwName = coins.map((el) => el.market).join();
+
   // 코인 현재 가격 가져오기
   const getPrice = async (name) => {
     const apiUrl = "https://api.upbit.com/v1/ticker?markets=";
     const priceUrl = apiUrl + name;
+    console.log(priceUrl);
 
     const response = await fetch(priceUrl);
-    const json = await response.json();
-    setPrice(json);
+    const priceJson = await response.json();
+    console.log(priceJson);
+    console.log(typeof priceJson);
+    const tradePrice = await priceJson.map((el) => el["trade_price"]);
+    setPrice(tradePrice);
+    console.log("");
   };
 
   useEffect(() => {
     getCoins();
-    getPrice("KRW-BTC");
   }, []);
-  console.log(price);
+
+  useEffect(() => {
+    getPrice(krwName);
+  }, [coins]);
 
   return (
     <div>
@@ -43,6 +52,9 @@ function Upbit() {
             <li key={coin.market}>
               {coin.market} {coin.korean_name} {coin.english_name}
             </li>
+          ))}
+          {price.map((el, index) => (
+            <li key={index}>{el}</li>
           ))}
         </ul>
       )}
